79e68d0074162a2f8ccfda65ef406402934a150f
jlogoz04 <jlogozzo147@gmail.com>
2025-08-09 23:35:23 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import sqlite3 from 'sqlite3';
 import basicAuth from 'express-basic-auth';
-import fs from 'fs';                 // <-- no $1
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
